1358dc75a71d9b38dc24d9f9164a27505644336c
Brandon Amos <bamos@cmu.edu>
2014-07-28 13:28:38 -0700

    Add Thrift schema.

---
+++ src/main/thrift/SampleThriftObject.thrift
@@ -0,0 +1,25 @@
+///////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2014 Adobe Systems Incorporated. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+///////////////////////////////////////////////////////////////////////////
+
+namespace java com.adobe.spark_parquet_thrift
+
+struct SampleThriftObject {
+  10: string col_a;
+  20: string col_b;
+  30: string col_c;
+}
